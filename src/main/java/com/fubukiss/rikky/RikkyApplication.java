44b0d8555c0bc0d9015c2cf4c9ddf606c71539b9
River <CryingRiver@foxmail.com>
2022-12-14 23:39:13 +0800

    添加自定义banner

---
+++ src/main/java/com/fubukiss/rikky/RikkyApplication.java
@@ -0,0 +1,22 @@
+package com.fubukiss.rikky;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>Project: rikky-takeaway - RikkyApplication
+ * <p>Powered by Riverify On 12-14-2022 23:28:13
+ *
+ * @author Riverify
+ * @version 1.0
+ * @since JDK8
+ */
+@Slf4j
+@SpringBootApplication
+public class RikkyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RikkyApplication.class, args);
+        log.info("项目启动成功");
+    }
+}
