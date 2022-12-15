2643d24bc8fb931fef2406783236cb3d31ea2414
River <CryingRiver@foxmail.com>
2022-12-15 19:19:54 +0800

    启动功能，增加R结果类

---
+++ src/main/java/com/fubukiss/rikky/common/R.java
@@ -0,0 +1,58 @@
+package com.fubukiss.rikky.common;
+
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * 通用返回结果类
+ * <p>服务端相应的数据最终都会封装成此对象
+ *
+ * @param <T>
+ */
+@Data
+public class R<T> {
+
+    /**
+     * 编码：1成功，0和其它数字为失败
+     */
+    private Integer code;
+
+    /**
+     * 错误信息
+     */
+    private String msg;
+
+    /**
+     * 具体数据
+     */
+    private T data;
+
+    /**
+     * 动态数据
+     */
+    private Map<T, T> map = new HashMap<>();
+
+
+    public static <T> R<T> success(T object) {
+        R<T> r = new R<T>();
+        r.data = object;
+        r.code = 1;
+        return r;
+    }
+
+    public static <T> R<T> error(String msg) {
+        R r = new R();
+        r.msg = msg;
+        r.code = 0;
+        return r;
+    }
+
+    public R<T> add(String key, Object value) {
+        this.map.put(key, value);
+        return this;
+    }
+
+}
