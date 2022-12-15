a70a78f091b0e82a20dc375232b96978c3ccfe4a
River <CryingRiver@foxmail.com>
2022-12-15 17:50:41 +0800

    登录功能：Employee实体类

---
+++ src/main/java/com/fubukiss/rikky/entity/Employee.java
@@ -0,0 +1,50 @@
+package com.fubukiss.rikky.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * 员工实体类
+ */
+@Data
+public class Employee implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String username;
+
+    private String name;
+
+    private String password;
+
+    private String phone;
+
+    private String sex;
+
+    /**
+     * 身份证号码 使用了驼峰命名法，和employee表中的id_number不同。
+     * <p>通过在 application.yml 的 mybatis-plus.configuration.map-underscore-to-camel-case 属性设置为 true
+     * Mybatis Plus 帮助我们自动实现驼峰命名向 underline 模式的转换。
+     * <p>之后的几个变量亦是如此。
+     */
+    private String idNumber;
+
+    private Integer status;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Long createUser;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateUser;
+
+}
