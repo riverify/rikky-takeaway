1c5871113dd6d5455ab8ae239b567162cf354b3d
River <CryingRiver@foxmail.com>
2022-12-15 19:03:39 +0800

    登录功能：Employee控制类的创建

--- src/main/java/com/fubukiss/rikky/controller/EmployeeController.java
+++ src/main/java/com/fubukiss/rikky/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.fubukiss.rikky.controller;
 
+import com.fubukiss.rikky.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
 }
