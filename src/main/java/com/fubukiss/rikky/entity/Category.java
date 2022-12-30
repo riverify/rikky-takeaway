ac87e592faf6941ce28b761dd2d5dd09368162e8
River <CryingRiver@foxmail.com>
2022-12-30 21:18:53 +0800

    Category.java实体类

---
+++ src/main/java/com/fubukiss/rikky/entity/Category.java
@@ -0,0 +1,56 @@
+package com.fubukiss.rikky.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * 分类
+ */
+@Data
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+
+    //类型 1 菜品分类 2 套餐分类
+    private Integer type;
+
+
+    //分类名称
+    private String name;
+
+
+    //顺序
+    private Integer sort;
+
+
+    //创建时间
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+
+    //更新时间
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+
+
+    //创建人
+    @TableField(fill = FieldFill.INSERT)
+    private Long createUser;
+
+
+    //修改人
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateUser;
+
+
+    //是否删除
+    private Integer isDeleted;
+
+}
