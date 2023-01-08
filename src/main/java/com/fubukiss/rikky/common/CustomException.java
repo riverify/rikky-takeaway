ce7b287659068b6c3e006072729b25b9fa198f48
river <CryingRiver@outlook.com>
2023-01-08 20:59:34 +0800

    自定义异常类

---
+++ src/main/java/com/fubukiss/rikky/common/CustomException.java
@@ -0,0 +1,16 @@
+package com.fubukiss.rikky.common;
+
+/**
+ * <p>Project: rikky-takeaway - CustomException 自定义异常类
+ * <p>Powered by river On 2023/01/08 8:52 PM
+ *
+ * @author Riverify
+ * @version 1.0
+ * @since JDK8
+ */
+public class CustomException extends RuntimeException {
+    public CustomException(String message) {
+        super(message);
+    }
+
+}
