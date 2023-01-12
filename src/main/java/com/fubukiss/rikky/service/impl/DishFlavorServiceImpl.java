6d1c61f98042d41df25cbd4d3cdb76090904d996
river <CryingRiver@outlook.com>
2023-01-12 14:49:47 +0800

    菜品添加相关的类和接口

--- src/main/java/com/fubukiss/rikky/service/impl/DishFlavorServiceImpl.java
+++ src/main/java/com/fubukiss/rikky/service/impl/DishFlavorServiceImpl.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.fubukiss.rikky.entity.DishFlavor;
 import com.fubukiss.rikky.mapper.DishFlavorMapper;
 import com.fubukiss.rikky.service.DishFlavorService;
+import org.springframework.stereotype.Service;
 
 /**
  * <p>Project: rikky-takeaway - DishFlavorServiceImpl
@@ -13,6 +14,7 @@ import com.fubukiss.rikky.service.DishFlavorService;
  * @version 1.0
  * @since JDK8
  */
+@Service
 public class DishFlavorServiceImpl extends ServiceImpl<DishFlavorMapper, DishFlavor> implements DishFlavorService {
     // ServiceImpl<DishFlavorMapper, DishFlavor> 为MyBatis-Plus提供的基础实现类，<DishFlavorMapper, DishFlavor> 为泛型，DishFlavorMapper为Mapper接口，DishFlavor为实体类
 }
